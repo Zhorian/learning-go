053a994cef3b81cfea903ba6d75df2caa97c2abe
Gareth <garethkear@outlook.com>
2025-01-05 14:25:45 +0000

    Added boilerplate for handlers

--- learn-web-services/reading-list/cmd/api/handlers.go
+++ learn-web-services/reading-list/cmd/api/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +18,50 @@ func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, "Display a list of books on the reading list")
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		fmt.Fprintf(w, "Add a book on the reading list")
+		return
+	}
+
+	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 }
 
 func (app *application) getUpdateDeleteCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		app.getBook(w, idInt)
+		return
+	case http.MethodPut:
+		app.updateBook(w, idInt)
+		return
+	case http.MethodDelete:
+		app.deleteBook(w, idInt)
+		return
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (app *application) getBook(w http.ResponseWriter, id int64) {
+	fmt.Fprintf(w, "Display book with id of %d", id)
+}
+
+func (app *application) updateBook(w http.ResponseWriter, id int64) {
+	fmt.Fprintf(w, "Update book with id of %d", id)
+}
+
+func (app *application) deleteBook(w http.ResponseWriter, id int64) {
+	fmt.Fprintf(w, "Delete book with id of %d", id)
 }
